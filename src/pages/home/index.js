071c341d4f19ba8b841f16c5ae14eb7d1cb891e2
Joao Martins <milanezijoao51@gmail.com>
2024-08-15 13:24:38 -0300

    feature:Input e button

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,21 @@
 import {Header} from '../../components/Header'
+import background from '../../Assets/image.png'
+
+import './styles.css'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className = 'content'>
+        <img src = {background} className= "background" alt = 'Background App'/>
+        <div className ="info">
+          <div>
+            <input name="user" placeholder="@username"></input>
+            <button>Buscar</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
